6776a963c970d01487ec56f5178f753b26491959
dpramono <RSG@yahoo.com>
2017-07-06 14:59:32 +0700

    Add project files.

---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports ClassLibrary1
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim add As New ClassLibrary1.MyFunctions
+        Label1.Text = add.AddMyValues(TextBox1.Text, TextBox2.Text).ToString
+        Dim ax As New ClassLibrary1.Form1
+        ax.Show()
+        ax.TextBox1.Text = Label1.Text
+    End Sub
+End Class
